fa3984c589339c4002e4937274e22ba0d7f22a5f
Aravind-Suresh <arvindsuresh2009@gmail.com>
2017-03-05 23:06:35 +0530

    Fixed main.cpp. Added tests.

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ typedef vector<vii > vvii;
 
 const int INF = (int)(1e9);
 
+// TODO: Assert in range ( 1, n )
 class Graph {
 private:
   int nodesCount;
@@ -36,9 +37,10 @@ private:
 public:
   Graph(int n) {
     nodesCount = n;
-    nodes.reserve(n+1);
-    adj.reserve(n+1);
-    betweennessWeights.reserve(n+1);
+    nodes.resize(n+1);
+    adj.resize(n+1);
+    betweennessWeights.resize(n+1);
+    edgesCount = 0;
   }
   void addEdge(int a, int b, int w) {
     // Directed edge from a->b with a weight w
@@ -46,34 +48,39 @@ public:
     ++edgesCount;
   }
   void print() {
-    cout<<"Graph details"<<endl;
-    cout<<"Nodes: "<<nodesCount<<"\tEdges: "<<edgesCount<<endl;
+    cout << "Graph info" << endl;
+    cout << "Nodes: " << nodesCount << "\tEdges: " << edgesCount << endl;
     int idx = 1;
-    for(vector<vector<pair<int, int> > >::iterator it = adj.begin(); it != adj.end(); ++it) {
+    for(vector<vector<pair<int, int> > >::iterator it = adj.begin() + 1; it != adj.end(); ++it) {
       vector<pair<int, int> > v = (*it);
       cout<<idx<<": ";
       for(vector<pair<int, int> >::iterator _it = v.begin(); _it != v.end(); ++_it) {
-        cout<<" "<<(_it->first);
+        cout << " " << (_it->first) << "," << (_it->second);
       }
       cout<<endl;
+      ++idx;
     }
   }
   int getBetweennessWeight(int u, int v) {
     return betweennessWeights[u][v];
   }
+  void setBetweennessWeight(int u, int v, int w) {
+    if(!betweennessWeights[u].size()) betweennessWeights[u].resize(nodesCount + 1);
+    betweennessWeights[u][v] = w;
+  }
   void shortestPath(int source, vector<int> & paths, vector<int> & is_leaf);
   int addWeights(int source, int node, vector<int> & paths, vector<int> & is_leaf);
-  int computeWeightedBetweennessCentrality(int v);
+  double computeWeightedBetweennessCentrality(int v);
 };
 
 void Graph::shortestPath(int source, vector<int> & paths, vector<int> & is_leaf) {
   priority_queue<pair<int, int> > pq;
   vector<int> dist(nodesCount + 1, INF);
   is_leaf.clear();
-  is_leaf.reserve(nodesCount + 1);
+  is_leaf.resize(nodesCount + 1);
   fill(is_leaf.begin(), is_leaf.end(), true);
   paths.clear();
-  paths.reserve(nodesCount + 1);
+  paths.resize(nodesCount + 1);
 
   pq.push(make_pair(0, source));
   dist[source] = 0;
@@ -121,16 +128,36 @@ int Graph::addWeights(int source, int node, vector<int> & paths, vector<int> & i
   return sum;
 }
 
-int Graph::computeWeightedBetweennessCentrality(int v) {
+double Graph::computeWeightedBetweennessCentrality(int v) {
   vector<int> paths, is_leaf;
-  int sum = 0;
+  double sum = 0;
   for(int i = 1; i <= nodesCount; ++i) {
     shortestPath(i, paths, is_leaf);
     sum += addWeights(i, v, paths, is_leaf);
   }
+  sum /= (nodesCount - 1)*(nodesCount - 2);
   return sum;
 }
 
 int main() {
+  int n, m, g;
+  cin >> n >> m >> g;
+  Graph graph(n);
+  int u, v, w;
+  for(int i = 0; i < m; ++i) {
+    cin >> u >> v >> w;
+    graph.addEdge(u, v, w);
+  }
+  int t = n*(n-1)/2;
+  for(int i = 1; i <= n; ++i) {
+    graph.setBetweennessWeight(i, i, 0);
+  }
+  for(int i = 0; i < t; ++i) {
+    cin >> u >> v >> w;
+    graph.setBetweennessWeight(u, v, w);
+    graph.setBetweennessWeight(v, u, w);
+  }
+  graph.print();
+  cout << "Betweenness centrality: " << fixed << graph.computeWeightedBetweennessCentrality(g) << endl;
   return 0;
 }
